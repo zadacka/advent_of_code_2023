f37a958a784820721945fcd58666e798d5c60d64
Alexander Zawadzki <alexander.zawadzki@gmail.com>
2024-03-03 11:06:33 +0000

    Day 7 - part 1 working

--- day07/day07.py
+++ day07/day07.py
@@ -22,42 +22,25 @@ def value_hand(hand):
     value_counts = Counter(card_counts.values())
     if 5 in value_counts:
         # Five of a kind, where all five cards have the same label: AAAAA
-        # values up to 5 * 14 + 262 = 332
-        card = max(card_counts.items(), key=operator.itemgetter(1))[0]
-        return card_to_value[card] + 332
+        return 7
     elif 4 in value_counts:
         # Four of a kind, where four cards have the same label and one card has a different label: AA8AA
-        # values up to 4 * 14 + 206 = 262
-        card = max(card_counts.items(), key=operator.itemgetter(1))[0]
-        return card_to_value[card] + 262
+        return 6
     elif 3 in value_counts and 2 in value_counts:
         # Full house, where three cards have the same label, and the remaining two cards share a different label: 23332
-        # values up to 3* 14 + 2 * 13 + 138 = 206
-        fh1 = max(card_counts.items(), key=operator.itemgetter(1))[0]
-        card_counts.__delitem__(fh1)
-        fh2 = max(card_counts.items(), key=operator.itemgetter(1))[0]
-        return card_to_value[fh1] + card_to_value[fh2] + 206
+        return 5
     elif 3 in value_counts:
         # Three of a kind, where three cards have the same label, and the remaining two cards are each different from any other card in the hand: TTT98
-        # values up to 3 * 14 + 96 = 138
-        card = max(card_counts.items(), key=operator.itemgetter(1))[0]
-        return card_to_value[card] + 262 + 138
+        return 4
     elif 2 in value_counts and 2 in value_counts.values():
         # Two pair, where two cards share one label, two other cards share a second label, and the remaining card has a third label: 23432
-        # values up to 2 * 14 + 2 * 13 + 42 = 96
-        pair1 = max(card_counts.items(), key=operator.itemgetter(1))[0]
-        card_counts.__delitem__(pair1)
-        pair2 = max(card_counts.items(), key=operator.itemgetter(1))[0]
-        return card_to_value[pair1] + card_to_value[pair2] + 96
+        return 3
     elif 2 in value_counts:
         # One pair, where two cards share one label, and the other three cards have a different label from the pair and each other: A23A4
-        # values up to 2 * 14 + 14 = 42
-        card = max(card_counts.items(), key=operator.itemgetter(1))[0]
-        return card_to_value[card] + 42
+        return 2
     else:
         # High card, where all cards' labels are distinct: 23456
-        # values up to 14
-        return max([card_to_value[card] for card in hand])
+        return 1
 
 
 def test_part1_test_input():
@@ -80,4 +63,19 @@ def test_part1_test_input():
         winning_total += hand_to_bid[hand] * rank
 
     # 765 * 1 + 220 * 2 + 28 * 3 + 684 * 4 + 483 * 5
+    assert winning_total == 6440
+
+def test_part1_real_input():
+    hands, bids = parse_input('day07_real_input.txt')
+    hand_to_bid = {hand: bid for hand, bid in zip(hands, bids)}
+    hand_to_value = {}
+    for hand in hands:
+        # first the hand value, then the card values for a tie-break
+        hand_to_value[hand] = [value_hand(hand)] + [card_to_value[card] for card in hand]
+
+    winning_order = sorted(hand_to_value.items(), key=operator.itemgetter(1))
+    winning_total = 0
+    for rank, (hand, score) in enumerate(winning_order, start=1):
+        winning_total += hand_to_bid[hand] * rank
+
     assert winning_total == 6440
